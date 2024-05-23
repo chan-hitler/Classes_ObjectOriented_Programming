95cf5a3cbf147b63331fcb65040e8ff3feddd9bc
chan-hitler <tryalanthomas@gmail.com>
2024-05-22 23:20:16 -0400

    Update 4.4.12.2 PizzaTIme! Tester.java

--- 4.4.12.2 PizzaTIme! Tester.java
+++ 4.4.12.2 PizzaTIme! Tester.java
@@ -4,8 +4,10 @@ public class PizzaTester extends ConsoleProgram
     {
         Pizza chicken = new Pizza("Square cut", "Chicken and Banana Peppers", 10);
         Pizza sausage = new Pizza("Pie cut", "Sausage and Pineapple", 12);
-        
+        Pizza pepperoni = new Pizza("Strip cut", "Pepperoni and Mushrooms", 8);
+
         System.out.println(chicken);
         System.out.println(sausage);
+        System.out.println(pepperoni);
     }
 }
